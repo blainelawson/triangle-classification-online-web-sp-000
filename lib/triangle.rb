46d66a83702ccbbe4d89a51a545703c83e65ec27
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-21 16:49:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    # :equilateral, :isosceles, :scalene
+
+    if @side_1 == @side_2 && @side_2 == @side_3
+      :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      :isosceles
+
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
